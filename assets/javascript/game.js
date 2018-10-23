e4062c7c7399b2cfa231480412524e48501e3edf
Speedway <harpalassi@gmail.com>
2018-10-22 21:53:34 -0500

    finished game mechanics

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var totalscore = 0;
 var randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
 var crystal1 = Math.floor(Math.random() * 12) + 1;
@@ -7,36 +7,69 @@ var crystal2 = Math.floor(Math.random() * 12) + 1;
 var crystal3 = Math.floor(Math.random() * 12) + 1;
 var crystal4 = Math.floor(Math.random() * 12) + 1;
 
+
+function crystalMethod() {
+    crystal1 = Math.floor(Math.random() * 12) + 1;
+    crystal2 = Math.floor(Math.random() * 12) + 1;
+    crystal3 = Math.floor(Math.random() * 12) + 1;
+    crystal4 = Math.floor(Math.random() * 12) + 1;
+}
+
 console.log(crystal1);
 console.log(crystal2);
 console.log(crystal3);
 console.log(crystal4);
-
-var randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
 console.log(randomNum);
 
-
-$(document).ready(function() {
+function gameStart() {
+    randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
     $("#random-num").text(randomNum);
-});
+    totalscore = 0;
+    $("#total-score").text(totalscore);
+    crystalMethod();   
+}
 
+function winOrLose() {
+    if (randomNum === totalscore) {
+        wins++;
+        console.log(wins);  
+        $("#wins").text(wins);
+        $("#score-board").text("YOU WIN");
+        gameStart();
+
+    } else if (totalscore > randomNum) {
+        losses++;
+        console.log(losses);
+        $("#losses").text(losses);
+        $("#score-board").text("YOU LOSE");
+        gameStart();
+    }
+}
 
 $("#crystal-1").on("click", function() {
    totalscore += crystal1;
    $("#total-score").text(totalscore);
+   winOrLose();
 });
 
 $("#crystal-2").on("click", function() {
    totalscore += crystal2;
    $("#total-score").text(totalscore);
+   winOrLose();
 });
 
 $("#crystal-3").on("click", function() {
    totalscore += crystal3;
    $("#total-score").text(totalscore);
+   winOrLose();
 });
 
 $("#crystal-4").on("click", function() {
    totalscore += crystal4;
    $("#total-score").text(totalscore);
+   winOrLose();
+});
+
+$(document).ready(function() {
+    gameStart();
 });
