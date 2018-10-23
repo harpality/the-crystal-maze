6bf2933a6bc1ae709a741b601aef38a039b6580e
Speedway <harpalassi@gmail.com>
2018-10-22 20:47:26 -0500

    added crystal scores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
 var wins;
 var losses;
+var totalscore = 0;
+var randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
+var crystal1 = Math.floor(Math.random() * 12) + 1;
+var crystal2 = Math.floor(Math.random() * 12) + 1;
+var crystal3 = Math.floor(Math.random() * 12) + 1;
+var crystal4 = Math.floor(Math.random() * 12) + 1;
 
-var randomCrystal = Math.floor(Math.random() * 12) + 1;
-console.log(randomCrystal);
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
 
 var randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
 console.log(randomNum);
@@ -12,3 +20,23 @@ $(document).ready(function() {
     $("#random-num").text(randomNum);
 });
 
+
+$("#crystal-1").on("click", function() {
+   totalscore += crystal1;
+   $("#total-score").text(totalscore);
+});
+
+$("#crystal-2").on("click", function() {
+   totalscore += crystal2;
+   $("#total-score").text(totalscore);
+});
+
+$("#crystal-3").on("click", function() {
+   totalscore += crystal3;
+   $("#total-score").text(totalscore);
+});
+
+$("#crystal-4").on("click", function() {
+   totalscore += crystal4;
+   $("#total-score").text(totalscore);
+});
