53c11af58ff87e6f950686b75212b7c113e78d4d
Speedway <harpalassi@gmail.com>
2019-03-25 15:27:29 -0500

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # The Crystal Maze
 
+_The Crystal Maze_ is a web game made with JavaScript, jQuery, HTML, Bootstrap and CSS3. I utilized these technologies to create a random number guessing game using click methods.
+
 ![screenshot](assets/images/screenshot.png)
 
-🚀 Deployed at: http://harpalassi.github.io/The-Crystal-Maze
+## Functionality
+
+You will be given a random number at the start of the game. There are four crystals. By clicking on a crystal you will add a specific amount of points to your total score.
+
+You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number.
+
+The value of each crystal is hidden from you until you click on it. Each time the game starts, the value of the crystal will change.
+
+🚀 Deployed at: http://harpalassi.github.io/the-crystal-maze
+
+## Built With
+
+- jQuery
+- JavaScript
+- Bootstrap 4
+- CSS3
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,107 +1,109 @@
-var wins = 0;
-var losses = 0;
-var crystal1 = 0
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
-var randomNum = 0;
-var totalscore = 0;
-var backgroundTune = document.getElementById("playMaze");
+// data needed to keep game count
 
+let wins = 0;
+let losses = 0;
+let crystal1 = 0;
+let crystal2 = 0;
+let crystal3 = 0;
+let crystal4 = 0;
+let randomNum = 0;
+let totalscore = 0;
 
-function crystalMethod() {
-    crystal1 = Math.floor(Math.random() * 12) + 1;
-    crystal2 = Math.floor(Math.random() * 12) + 1;
-    crystal3 = Math.floor(Math.random() * 12) + 1;
-    crystal4 = Math.floor(Math.random() * 12) + 1;
-}
+// background music
+const backgroundTune = document.getElementById('playMaze');
 
-function winSound() {
-    document.getElementById('audiotag1').play();
-}
+// function to assign random numbers to crystals
+const crystalMethod = () => {
+  crystal1 = Math.floor(Math.random() * 12) + 1;
+  crystal2 = Math.floor(Math.random() * 12) + 1;
+  crystal3 = Math.floor(Math.random() * 12) + 1;
+  crystal4 = Math.floor(Math.random() * 12) + 1;
+};
 
-function lossSound() {
-    document.getElementById('audiotag2').play();
-}
+// win and loss sounds
+const winSound = () => {
+  document.getElementById('audiotag1').play();
+};
 
-function playBkgrd() {
-    backgroundTune.addEventListener("click", function(){
-    document.getElementById("audiotag3").muted = false;
-    document.getElementById("audiotag3").play();
-    document.getElementById("audiotag3").loop = true;
+const lossSound = () => {
+  document.getElementById('audiotag2').play();
+};
+
+// plays background music
+const playBkgrd = () => {
+  backgroundTune.addEventListener('click', function() {
+    document.getElementById('audiotag3').muted = false;
+    document.getElementById('audiotag3').play();
+    document.getElementById('audiotag3').loop = true;
   });
-}
-
-function stopBkgrd() {
-    document.getElementById("stopMaze").addEventListener("click", function(){
-    document.getElementById("audiotag3").muted = true;
-  })
 };
 
-function gameStart() {
-    randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
-    $("#random-num").text(randomNum);
-    totalscore = 0;
-    updateScore();
-    playBkgrd();
-    stopBkgrd();
-    crystalMethod();   
-}
-
-function updateScore() {
-    $("#total-score").text(totalscore);
+// stops background music
+const stopBkgrd = () => {
+  document.getElementById('stopMaze').addEventListener('click', function() {
+    document.getElementById('audiotag3').muted = true;
+  });
 };
 
-function winOrLose() {
-    if (randomNum === totalscore) {
-        wins++;
-        console.log(wins);  
-        $("#wins").text(wins);
-        $("#score-board").text("YOU WIN");
-        winSound();
-        gameStart();
+// starts the game and selects random number
+const gameStart = () => {
+  randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
+  $('#random-num').text(randomNum);
+  totalscore = 0;
+  updateScore();
+  playBkgrd();
+  stopBkgrd();
+  crystalMethod();
+};
 
-    } else if (totalscore > randomNum) {
-        losses++;
-        console.log(losses);
-        $("#losses").text(losses);
-        $("#score-board").text("YOU LOSE");
-        lossSound();
-        gameStart();
-    }
-}
+// shows new score on the DOM
+const updateScore = () => {
+  $('#total-score').text(totalscore);
+};
 
-$(document).ready(function() {
+// logic for winning or losing the game
+const winOrLose = () => {
+  if (randomNum === totalscore) {
+    wins++;
+    $('#wins').text(wins);
+    $('#score-board').text('YOU WIN');
+    winSound();
     gameStart();
+  } else if (totalscore > randomNum) {
+    losses++;
+    $('#losses').text(losses);
+    $('#score-board').text('YOU LOSE');
+    lossSound();
+    gameStart();
+  }
+};
+
+// start the game when document has loaded
+$(document).ready(() => {
+  gameStart();
 });
 
-
-$("#crystal-1").on("click", function() {
-   totalscore += crystal1;
-   updateScore();
-   winOrLose();
+// click methods for each crystal
+$('#crystal-1').on('click', () => {
+  totalscore += crystal1;
+  updateScore();
+  winOrLose();
 });
 
-$("#crystal-2").on("click", function() {
-   totalscore += crystal2;
-   updateScore();
-   winOrLose();
+$('#crystal-2').on('click', () => {
+  totalscore += crystal2;
+  updateScore();
+  winOrLose();
 });
 
-$("#crystal-3").on("click", function() {
-   totalscore += crystal3;
-   updateScore();
-   winOrLose();
+$('#crystal-3').on('click', () => {
+  totalscore += crystal3;
+  updateScore();
+  winOrLose();
 });
 
-$("#crystal-4").on("click", function() {
-   totalscore += crystal4;
-   updateScore();
-   winOrLose();
+$('#crystal-4').on('click', () => {
+  totalscore += crystal4;
+  updateScore();
+  winOrLose();
 });
-
-console.log(crystal1);
-console.log(crystal2);
-console.log(crystal3);
-console.log(crystal4);
-console.log(randomNum);
